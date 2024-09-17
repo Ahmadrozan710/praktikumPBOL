f8dddf885c3805172b2ded368fda909df1fc4b24
Ahmadrozan710 <151592014+Ahmadrozan710@users.noreply.github.com>
2024-09-17 11:56:55 +0700

    Add files via upload

---
+++ Tugas 1/TugasJava1.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class TugasJava1 {
+
+    static String nama;
+    static int nim;
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    static void TambahDataMahasiswa() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Masukkan Nama : ");
+        nama = scan.nextLine();
+        System.out.print("Masukkan NIM  : ");
+        nim = scan.nextInt();
+        scan.nextLine();
+
+    }
+
+    static void DaftarMahasiswa() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM  : " + nim);
+
+        char pilih;
+        System.out.println("Tekan Enter Untuk Melanjutkan");
+        scan.nextLine();
+
+    }
+
+    public static void main(String[] args) {
+        clearScreen();
+        Scanner scan = new Scanner(System.in);
+        int pilihan = 0;
+
+        // Menggunakan while untuk terus menampilkan menu
+        while (pilihan != 3) {
+            clearScreen();
+
+            System.out.println("Data Mahasiswa");
+            System.out.println("1. Tambah Data Mahasiswa");
+            System.out.println("2. Daftar mahasiswa");
+            System.out.println("3. Keluar");
+            System.out.print("Pilihan: ");
+            pilihan = scan.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    clearScreen();
+                    TambahDataMahasiswa();
+                    break;
+                case 2:
+                    clearScreen();
+                    DaftarMahasiswa();
+                    break;
+                case 3:
+                    clearScreen();
+                    System.out.println("Terima kasih");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid, coba lagi.");
+                    break;
+            }
+        }
+    }
+}
